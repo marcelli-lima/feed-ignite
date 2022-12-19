f40f3ef75fd6d075ed5d55c27697eabaa2e10b8b
marcelli-lima <marcelli_lima@hotmail.com>
2022-12-19 17:38:18 -0300

    feed ignite com typescript

---
+++ 01-fundamentos-reactjs-ts/src/components/Sidebar.tsx
@@ -0,0 +1,28 @@
+import { PencilLine } from "phosphor-react";
+import { Avatar } from "./Avatar";
+
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://images.unsplash.com/photo-1525373698358-041e3a460346?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://github.com/marcelli-lima.png" />
+        <strong>Marcelli Lima</strong>
+        <span>Web Developer</span>
+      </div>
+
+      <footer>
+        <a href="#">
+          <PencilLine size={20} />
+          Editar seu perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
